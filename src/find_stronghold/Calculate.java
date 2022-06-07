d2a1bb644598a759cd77cf9f77ba4e0a4d6c63c8
AlexCai2019 <alexcai910630@gmail.com>
2022-06-07 13:48:53 +0800

    最佳化儲存與計算座標部分的程式碼

--- src/find_stronghold/Calculate.java
+++ src/find_stronghold/Calculate.java
@@ -2,54 +2,66 @@ package find_stronghold;
 
 public class Calculate
 {
-    public String findStronghold(String[][] eyePaths)
+    private final Coordinate[] line1 = new Coordinate[]{ new Coordinate(0, 0),new Coordinate(0, 0) }; //第一條線
+    private final Coordinate[] line2 = new Coordinate[]{ new Coordinate(0, 0),new Coordinate(0, 0) }; //第二條線
+    private final Coordinate[][] lines = { line1,line2 };
+    private boolean startCalculate = false;
+
+    public String findStronghold(String singleXOrZ, int row, int eitherXOrZ)
     {
-        Coordinate[] line1; //第一條線
-        Coordinate[] line2; //第二條線
+        if (singleXOrZ.isEmpty())
+            return ""; //如果字串是空的 就回傳空字串
 
         try //將輸入的字串們轉為線上的兩點
         {
-            line1 = new Coordinate[]
-            {
-                    new Coordinate(Double.parseDouble(eyePaths[0][0]), Double.parseDouble(eyePaths[0][1])),
-                    new Coordinate(Double.parseDouble(eyePaths[1][0]), Double.parseDouble(eyePaths[1][1]))
-            };
-            line2 = new Coordinate[]
-            {
-                    new Coordinate(Double.parseDouble(eyePaths[2][0]), Double.parseDouble(eyePaths[2][1])),
-                    new Coordinate(Double.parseDouble(eyePaths[3][0]), Double.parseDouble(eyePaths[3][1]))
-            };
+            //先判斷是哪一條方程式 此時使用[row >> 1] 決定是line1或line2
+            //因為row只會傳入0,1,2,3 因此右位移 0和1會變成0 2和3會變成1
+            //就可以找出是line1或是line2 (line1是前兩列連成的線 line2是後兩列連成的線)
+            //找到是line1或line2後 使用row & 1 找出是line1[0]或line1[1] (或是line2[0]/line2[1])
+            //因為row & 1 會將0和2轉換為0 並將1和3轉換為1
+            //抓到正確的那組Coordinate後 呼叫set方法傳入eitherXOrZ和Double.parseDouble(singleXOrZ)
+            //因為eitherXOrZ 這個數字是代表要修改的是x或是z
+            //而singleXOrZ 當初是傳入那個輸入框的內容
+            lines[row >> 1][row & 1].set(eitherXOrZ, Double.parseDouble(singleXOrZ));
         }
         catch (NumberFormatException numberFormatException)
         {
             return "請勿輸入數字以外的內容";
         }
 
+        if (!startCalculate)
+            return "";
+
         Coordinate[] ab = { calculateAB(line1), calculateAB(line2) }; //算出聯立方程式的AB
 
-        double[] stronghold = calculateXY(ab).getDoubleArray(); //算出聯立方程式的XY 並轉為double array
+        double[] stronghold = calculateXZ(ab).getDoubleArray(); //算出聯立方程式的XY 並轉為double array
 
         return String.format("[%.3f, %.3f]", stronghold[0], stronghold[1]);
+    }
+
+    public void setStartCalculate(boolean set)
+    {
+        startCalculate = set;
     }
 
     //找出AB
     private Coordinate calculateAB(Coordinate[] line)
     {
-        // y0 = a * x0 + b
-        // y1 = a * x1 + b
-        double a = (line[0].getY() - line[1].getY()) / (line[0].getX() - line[1].getX());
-        double b = line[0].getY() - a * line[0].getX();
+        // z0 = a * x0 + b
+        // z1 = a * x1 + b
+        double a = (line[0].getZ() - line[1].getZ()) / (line[0].getX() - line[1].getX());
+        double b = line[0].getZ() - a * line[0].getX();
         return new Coordinate(a, b);
     }
 
-    //找出XY
-    public Coordinate calculateXY(Coordinate[] ab)
+    //找出XZ
+    public Coordinate calculateXZ(Coordinate[] ab)
     {
-        // y = a0 * x + b0
-        // y = a1 * x + b1
+        // z = a0 * x + b0
+        // z = a1 * x + b1
         double x = -(ab[0].getB() - ab[1].getB()) / (ab[0].getA() - ab[1].getA());
-        double y = ab[0].getA() * x + ab[0].getB();
-        return new Coordinate(x, y);
+        double z = ab[0].getA() * x + ab[0].getB();
+        return new Coordinate(x, z);
     }
 }
 
@@ -57,10 +69,15 @@ class Coordinate
 {
     private final double[] coordinate = new double[2];
 
-    public Coordinate(double x, double y)
+    public Coordinate(double x, double z)
     {
         coordinate[0] = x;
-        coordinate[1] = y;
+        coordinate[1] = z;
+    }
+
+    public void set(int which, double value)
+    {
+        coordinate[which] = value;
     }
 
     public double getX()
@@ -72,7 +89,7 @@ class Coordinate
         return coordinate[0];
     }
 
-    public double getY()
+    public double getZ()
     {
         return coordinate[1];
     }
--- src/find_stronghold/MainWindow.java
+++ src/find_stronghold/MainWindow.java
@@ -5,14 +5,14 @@ import java.awt.event.*;
 
 public class MainWindow
 {
-    private final Frame frame;
+    private final Frame frame = new Frame("終界祭壇尋找器");
     private final TextField[] firstThrowTextFields = new TextField[2]; //第一組丟出的座標
     private final TextField[] firstLandTextFields = new TextField[2]; //第一組落地的座標
     private final TextField[] secondThrowTextFields = new TextField[2]; //第二組丟出的座標
     private final TextField[] secondLandTextFields = new TextField[2]; //第二組落地的座標
     private final TextField[][] allTextFields = { firstThrowTextFields,firstLandTextFields,secondThrowTextFields,secondLandTextFields };
     private final Font font = new Font("Serif", Font.PLAIN, 18); //字型
-    private final MyLabel resultLabel = new MyLabel("可能位置: ", new Font("Serif", Font.PLAIN, 24));
+    private final LabelWithFont resultLabel = new LabelWithFont("可能位置: ", new Font("Serif", Font.PLAIN, 24));
     private final String[][] coordinates = new String[4][2];
     private final Calculate calculate = new Calculate();
 
@@ -20,7 +20,6 @@ public class MainWindow
     {
         initialTextFields();
 
-        frame = new Frame("終界祭壇尋找器");
         frame.setSize(480, 340);
         frame.addWindowListener(new WindowAdapter()
         {
@@ -41,25 +40,25 @@ public class MainWindow
 
         FlowLayout flowLayout = new FlowLayout(FlowLayout.LEFT, 15, 1);
         Panel firstThrowPanel = new Panel(flowLayout);
-        firstThrowPanel.add(new MyLabel("請輸入第一組起始座標", font));
+        firstThrowPanel.add(new LabelWithFont("請輸入第一組起始座標", font));
         firstThrowPanel.add(firstThrowTextFields[0]);
         firstThrowPanel.add(firstThrowTextFields[1]);
         centerPanel.add(firstThrowPanel);
 
         Panel firstLandPanel = new Panel(flowLayout);
-        firstLandPanel.add(new MyLabel("請輸入第一組落地座標", font));
+        firstLandPanel.add(new LabelWithFont("請輸入第一組落地座標", font));
         firstLandPanel.add(firstLandTextFields[0]);
         firstLandPanel.add(firstLandTextFields[1]);
         centerPanel.add(firstLandPanel);
 
         Panel secondThrowPanel = new Panel(flowLayout);
-        secondThrowPanel.add(new MyLabel("請輸入第二組起始座標", font));
+        secondThrowPanel.add(new LabelWithFont("請輸入第二組起始座標", font));
         secondThrowPanel.add(secondThrowTextFields[0]);
         secondThrowPanel.add(secondThrowTextFields[1]);
         centerPanel.add(secondThrowPanel);
 
         Panel secondLandPanel = new Panel(flowLayout);
-        secondLandPanel.add(new MyLabel("請輸入第二組落地座標", font));
+        secondLandPanel.add(new LabelWithFont("請輸入第二組落地座標", font));
         secondLandPanel.add(secondLandTextFields[0]);
         secondLandPanel.add(secondLandTextFields[1]);
         centerPanel.add(secondLandPanel);
@@ -71,36 +70,45 @@ public class MainWindow
 
     private void initialTextFields()
     {
-        TextListener listener = e -> //輸入文字 textValueChanged(TextEvent e)
-        {
-            for (int i = 0; i < 4; i++)
-            {
-                for (int j = 0; j < 2; j++)
-                {
-                    coordinates[i][j] = allTextFields[i][j].getText();
-                    if (coordinates[i][j].isEmpty()) //還有空的輸入框
-                        return;
-                }
-            }
-
-            resultLabel.setText("可能位置: " + calculate.findStronghold(coordinates));
-        };
-
         for (int i = 0; i < 4; i++)
         {
             for (int j = 0; j < 2; j++)
             {
                 allTextFields[i][j] = new TextField(6);
                 allTextFields[i][j].setFont(font);
-                allTextFields[i][j].addTextListener(listener);
+
+                int row = i;
+                int eitherXOrZ = j;
+                allTextFields[i][j].addTextListener(e ->
+                {
+                    //走訪全部的輸入框
+                    for (int m = 0; m < 4; m++)
+                    {
+                        for (int n = 0; n < 2; n++)
+                        {
+                            coordinates[m][n] = allTextFields[m][n].getText();
+                            if (coordinates[m][n].isEmpty()) //還有空的輸入框
+                            {
+                                calculate.setStartCalculate(false);
+                                //當startCalculate為false時 呼叫findStronghold方法不做計算
+                                //只做將字串儲存進陣列的動作 以及過濾非數字的情形
+                                resultLabel.setText("可能位置: " + calculate.findStronghold(coordinates[row][eitherXOrZ], row, eitherXOrZ));
+                                return;
+                            }
+                        }
+                    }
+
+                    calculate.setStartCalculate(true);
+                    resultLabel.setText("可能位置: " + calculate.findStronghold(coordinates[row][eitherXOrZ], row, eitherXOrZ));
+                });
             }
         }
     }
 }
 
-class MyLabel extends Label
+class LabelWithFont extends Label
 {
-    public MyLabel(String text, Font font)
+    public LabelWithFont(String text, Font font)
     {
         super(text, LEFT);
         setFont(font);
